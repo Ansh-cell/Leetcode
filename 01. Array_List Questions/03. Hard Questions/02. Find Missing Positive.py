c942557acd1f87a534c87515aa88b9135a7aa098
Ansh Patel aka NightInGale <patelansh883@gmail.com>
2021-12-30 03:04:37 +0530

    Added Answer

---
+++ 01. Array_List Questions/03. Hard Questions/02. Find Missing Positive.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    # https://leetcode.com/problems/first-missing-positive/
+
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        l = len(nums)
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                nums[i] = 0
+
+        for i in range(len(nums)):
+            val = abs(nums[i])
+            if 1 <= val <= len(nums):
+                if nums[val - 1] > 0:
+                    nums[val - 1] *= -1
+                elif nums[val - 1] == 0:
+                    nums[val - 1] = -1 * (len(nums) + 1)
+
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] >= 0:
+                return i
+        return len(nums) + 1
+
+    # Time Complexity: 0(n), Space Complexity: O(1)
