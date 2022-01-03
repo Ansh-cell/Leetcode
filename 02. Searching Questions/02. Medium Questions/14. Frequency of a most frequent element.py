01ffecb17222c72a4de49a395eae5fd6cc896658
Ansh Patel aka NightInGale <patelansh883@gmail.com>
2022-01-03 21:18:11 +0530

    Added Answer

---
+++ 02. Searching Questions/02. Medium Questions/14. Frequency of a most frequent element.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    # https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+
+        nums.sort()
+
+        left, right = 0, 0
+        total, result = 0, 0
+
+        while right < len(nums):
+            total += nums[right]
+
+            while nums[right] * (right - left + 1) > total + k:
+                total -= nums[left]
+                left += 1
+
+            result = max(result, right - left + 1)
+            right += 1
+
+        return result
+
+    # Time Complexity: O(n * logn), Space Complexity: O(1)
